658d12eb54ac64a11896568a922c320a6fb6bb8f
Praveen Bhutani <bhutanipraveen74@gmail.com>
2026-07-28 16:42:36 +0530

    add

---
+++ dist/server/assets/_tanstack-start-manifest_v-Bki4BSnF.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/vendorInfra/src/routes/__root.tsx", children: ["/", "/$", "/api/career", "/api/contact", "/api/contact-sales", "/api/demo-bookings", "/api/investor", "/api/quote-requests", "/api/referral"], preloads: ["/assets/index-Co4yv1iN.js"] }, "/": { filePath: "D:/vendorInfra/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-yLvDZBsz.js", "/assets/ClientApp-MeNcPnB1.js"] }, "/$": { filePath: "D:/vendorInfra/src/routes/$.tsx", children: void 0, preloads: ["/assets/_-yLvDZBsz.js", "/assets/ClientApp-MeNcPnB1.js"] } }, clientEntry: "/assets/index-Co4yv1iN.js" });
+export {
+  tsrStartManifest
+};
